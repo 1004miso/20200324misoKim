16488045b1f47c6086203194573972c845be0ae4
1004c <1004c@DESKTOP-1VNCBGT>
2020-03-23 23:48:14 +0900

    객체배열 실습 4번문제
    1. Member 클래스 작성 : 회원 정보 클래스 작성.

---
+++ src/member/model/vo/Member.java
@@ -0,0 +1,62 @@
+package member.model.vo;
+
+public class Member {
+	private String userId;
+	private String userPwd;
+	private String userName;
+	private int age;
+	private char gender;
+	private String email;
+	
+	public Member() {}
+	public Member(String userId, String userPwd, String userName, int age, char gender, String email){
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.age = age;
+		this.gender = gender;
+		this.email = email;
+	}
+	
+	
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String get() {
+		return userPwd;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+		
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	
+}
---
+++ src/member/view/MemberMenu.java
@@ -0,0 +1,24 @@
+package member.view;
+
+import java.util.Scanner;
+
+import member.controller.MemberManager;
+
+public class MemberMenu {
+	private Scanner sc =new Scanner(System.in);
+	MemberManager mm = new MemberManager();
+	
+	public MemberMenu() {}
+	
+	public void mainMenu(){
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
