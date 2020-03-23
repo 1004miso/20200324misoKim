948549392f1010396a724ecb46957f0b489567c7
1004c <1004c@DESKTOP-1VNCBGT>
2020-03-24 00:20:55 +0900

    객체배열 실습 4번문제
    2. MemberMenu 클래스 작성

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -2,4 +2,21 @@ package member.controller;
 
 public class MemberManager {
 
+	
+	
+	
+	
+	
+	public void insertMember(){}
+	public void printAllMember() {}
+	public void searchId(){}
+	public void searchName() {}
+	public void searchEmail() {}
+	
+	public void updatePwd(){}
+	public void updateName(){}
+	public void updateEmail(){}
+	public void deleteOne(){}
+	public void deleteAll(){}
+	
 }
